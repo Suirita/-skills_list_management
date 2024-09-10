8f1d1d132141a2e6f23d78908b9c857685f9847b
Suirita Fahd <suirita.fahd.solicode@gmail.com>
2024-09-10 16:01:34 +0100

    algorithme exercice

---
+++ exercice/Exercice_7.js
@@ -0,0 +1,11 @@
+function function_7() {
+  const numbers = [1, 2, 3, 4];
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    sum += number;
+  }
+  console.log("La solution est :", sum);
+}
+
+function_7();
